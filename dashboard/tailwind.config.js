8c7c0b8722317b3f80a367a11ebbad46cf2b2e44
Leonardo Cunha <leonardo.cunha@comunix.net.br>
2023-07-12 15:48:08 -0300

    Configura router e tailwind

--- dashboard/tailwind.config.js
+++ dashboard/tailwind.config.js
@@ -1,11 +1,21 @@
 /** @type {import('tailwindcss').Config} */
+import palette from './palette'
+
 export default {
   content: [
     "./index.html",
     "./src/**/*.{vue,js,ts,jsx,tsx}"
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        regular: ['RobotoRegular'],
+        medium: ['RobotoMedium'],
+        bold: ['RobotoBold'],
+        black: ['RobotoBlack'],
+      },
+      colors: palette
+    },
   },
   plugins: [],
 }
